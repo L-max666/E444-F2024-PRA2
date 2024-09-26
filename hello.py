bcc07698edf87b003aaaa3b79283f76eb1f39b3d
liliuyue <liliuyue12@gmail.com>
2024-09-26 15:54:02 -0400

    website wo the date is completed

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
-# This is a simple Python script
-import flask 
-from flask import Flask
+# This is a simple Python script 
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
 
 app = Flask(__name__) 
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index() :
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name = name )
+
+if __name__ == '__main__':
+    app.run(debug=True) 
