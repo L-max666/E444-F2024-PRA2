37ceafe10f5e68fc03271e5f20d41e1f93f80cc5
liliuyue <liliuyue12@gmail.com>
2024-09-26 16:25:44 -0400

    time completed

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 # This is a simple Python script 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from datetime import datetime
 
 
 app = Flask(__name__) 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index() :
@@ -13,7 +15,8 @@ def index() :
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name )
+    
+    return render_template('user.html', name = name,current_time = datetime.utcnow() )
 
 if __name__ == '__main__':
     app.run(debug=True) 
