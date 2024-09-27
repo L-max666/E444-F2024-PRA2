e17fe50ce64e3ab5df7517eb15979bdb78a179dc
liliuyue <liliuyue12@gmail.com>
2024-09-26 23:14:54 -0400

    act 1.4 done

--- hello.py
+++ hello.py
@@ -1,22 +1,50 @@
 # This is a simple Python script 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, ValidationError
 
+app = Flask(__name__)
 
-app = Flask(__name__) 
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index() :
-    return render_template('index.html')
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if field.data and not field.data.endswith("utoronto.ca"):
+            raise ValidationError("The email you entered is not a UofT email!")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    email = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Looks like you have change you name')
+        email = form.email.data
+        form.email.data = ''
+        flash('Looks like you have change you email')
+
+    return render_template('index.html', form=form, name=name, email=email)
+
 
 @app.route('/user/<name>')
 def user(name):
-    
-    return render_template('user.html', name = name,current_time = datetime.utcnow() )
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
